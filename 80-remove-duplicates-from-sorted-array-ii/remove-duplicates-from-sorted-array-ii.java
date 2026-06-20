edbd8eae5e35a99ec2312d91c126596c3160382b
mayankjain1025 <mayankjai2023@gmail.com>
2026-06-20 22:57:42 +0530

    Time: 0 ms (100.00%) | Memory: 49.1 MB (14.46%) - LeetSync

--- 80-remove-duplicates-from-sorted-array-ii/remove-duplicates-from-sorted-array-ii.java
+++ 80-remove-duplicates-from-sorted-array-ii/remove-duplicates-from-sorted-array-ii.java
@@ -5,17 +5,17 @@ class Solution {
         int count = 0;
         
         for (int i = 0; i < n; i++) {
-            // Check if it's the very first element, OR a new number
+            
             if (j == 0 || nums[i] != nums[j - 1]) {
-                count = 1;          // Reset count to 1 for the new number
-                nums[j] = nums[i];  // Place the new number
-                j++;                // Move destination pointer
+                count = 1;          
+                nums[j] = nums[i];  
+                j++;                
             } 
-            // Otherwise, it's a duplicate. Only add it if count is less than 2
+           
             else if (count < 2) {
-                count++;            // Increment our duplicate counter
-                nums[j] = nums[i];  // Place the allowed duplicate
-                j++;                // Move destination pointer
+                count++;            
+                nums[j] = nums[i]; 
+                j++;                
             }
         }
         
