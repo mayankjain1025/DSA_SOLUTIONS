2335b441c5a495fececaecdeedf621fbf5a72d6a
mayankjain1025 <mayankjai2023@gmail.com>
2026-08-29 23:08:50 +0530

    Time: 1 ms (100.00%) | Memory: 48.3 MB (81.50%) - LeetSync

--- 4271-find-the-degree-of-each-vertex/find-the-degree-of-each-vertex.java
+++ 4271-find-the-degree-of-each-vertex/find-the-degree-of-each-vertex.java
@@ -1,12 +1,17 @@
 class Solution {
     public int[] findDegrees(int[][] matrix) {
-        int n=matrix.length;
-        int [] m =new int [n];
-        for(int i=0; i<n; i++){
-            for(int j=0; j<n; j++){
-                m[j]=m[j]+matrix[i][j];
+        int n = matrix.length;
+        int[] ans = new int[n];
+        
+        // The degree of a vertex in an adjacency matrix is the sum of its row (or column)
+        for (int i = 0; i < n; i++) {
+            int degree = 0;
+            for (int j = 0; j < n; j++) {
+                degree += matrix[i][j];
             }
+            ans[i] = degree;
         }
-        return m;
+        
+        return ans;
     }
 }
